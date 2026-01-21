536c42dcffc513921ad5d42552a057dfd8c54434
Volodymyr Mykychak <leadervlo.d@gmail.com>
2026-01-21 23:34:17 +0200

    impl: ticket booking system using semaphore

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,21 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private static final String SUCCESS_MESSAGE = "Booking successful.";
+    private static final String FAILURE_MESSAGE = "No seats available.";
+    private final Semaphore semaphore;
 
     public TicketBookingSystem(int totalSeats) {
-
+        // Додаємо true для "чесності", щоб люди в черзі проходили по порядку
+        this.semaphore = new Semaphore(totalSeats, true);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        if (semaphore.tryAcquire()) {
+            return new BookingResult(user, true, SUCCESS_MESSAGE);
+        }
+        return new BookingResult(user, false, FAILURE_MESSAGE);
     }
 }
